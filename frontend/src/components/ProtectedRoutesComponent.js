d8db2e06dee02586e2a7b82b90e1aa8c321a4989
Subash <subash.chinnannan@happiestminds.com>
2023-07-05 17:44:42 +0530

    deploy

---
+++ frontend/src/components/ProtectedRoutesComponent.js
@@ -0,0 +1,63 @@
+import { Outlet, Navigate } from "react-router-dom";
+import UserChatComponent from "./user/UserChatComponent";
+
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import LoginPage from "../pages/LoginPage";
+
+const ProtectedRoutesComponent = ({ admin }) => {
+  const [isAuth, setIsAuth] = useState();
+
+  useEffect(() => {
+    let cancelToken;
+    const source = axios.CancelToken.source();
+    const fetchData = async () => {
+      try {
+        // cancelToken = source.token;
+
+        const response = await axios.get("/api/get-token", {
+          cancelToken: source.token,
+        });
+
+        if (response.data.token) {
+          setIsAuth(response.data.token);
+        }
+      } catch (error) {
+        if (axios.isCancel(error)) {
+          // Request was canceled, no action needed
+        } else {
+          console.error("Error fetching data:", error);
+        }
+      }
+    };
+
+    fetchData();
+
+    return () => {
+      // Cancel the Axios request when the component is unmounted
+      if (cancelToken) {
+        cancelToken.cancel("Request canceled");
+      }
+    };
+  }, [isAuth]);
+  //   console.log("---->", isAuth);
+
+  if (isAuth === undefined) {
+    return <LoginPage />;
+  }
+
+  return isAuth && admin && isAuth !== "admin" ? (
+    <Navigate to="/" />
+  ) : isAuth && admin ? (
+    <Outlet />
+  ) : isAuth && !admin ? (
+    <>
+      <UserChatComponent />
+      <Outlet />
+    </>
+  ) : (
+    <Navigate to="/login" />
+  );
+};
+
+export default ProtectedRoutesComponent;
---
+++ frontend/src/pages/admin/utils/utils.js
@@ -0,0 +1,37 @@
+import axios from "axios";
+
+export const uploadImagesApiRequest = async (images, productId) => {
+  const formData = new FormData();
+  Array.from(images).forEach((image) => {
+    formData.append("images", image);
+  });
+  const { data } = await axios.post(
+    "/api/products/admin/upload?productId=" + productId,
+    formData
+  );
+  return data;
+};
+
+export const uploadImagesCloudinaryApiRequest = (images, productId) => {
+  const url = "https://api.cloudinary.com/v1_1/dscgzlscl/image/upload";
+  const formData = new FormData();
+  for (let i = 0; i < images.length; i++) {
+    let file = images[i];
+    formData.append("file", file);
+    formData.append("upload_preset", "yo9o1kcr");
+    fetch(url, {
+      method: "POST",
+      body: formData,
+    })
+      .then((response) => {
+        console.log(response);
+        return response.json();
+      })
+      .then((data) => {
+        axios.post(
+          "/api/products/admin/upload?cloudinary=true&productId=" + productId,
+          data
+        );
+      });
+  }
+};
