dfb343f3517180de3b3e49792b3963203c1c2031
Subash <subash.chinnannan@happiestminds.com>
2023-09-04 22:56:09 +0530

    Order model has been changed so that, it will be simple to store orders

--- backend/models/OrderModel.js
+++ backend/models/OrderModel.js
@@ -1,59 +1,50 @@
-const mongoose = require("mongoose")
-const User = require("./UserModel")
+const mongoose = require("mongoose");
+const User = require("./UserModel");
 
-const orderSchema = mongoose.Schema({
+const orderSchema = mongoose.Schema(
+  {
     user: {
-        type: mongoose.Schema.Types.ObjectId,
-        required: true,
-        ref: User,
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+      ref: User,
     },
-    orderTotal: {
-        itemsCount: {type: Number, required: true},
-        cartSubtotal: {type: Number, required: true}
-    },
-    cartItems: [
-        {
-            name: {type: String, required: true},
-            price: {type: Number, required: true},
-            image: {path: {type: String, required: true}},
-            quantity: {type: Number, required: true},
-            count: {type: Number, required: true}
-        }
-    ],
+    total: { type: Number, required: true },
+    quantity: { type: Number, required: true },
+    name: { type: String, required: true },
+    price: { type: Number, required: true },
+    image: { path: { type: String, required: true } },
+    count: { type: Number, required: true },
+
     paymentMethod: {
       type: String,
       required: true,
     },
-    transactionResult: {
-        status: {type: String},
-        createTime: {type: String},
-        amount: {type: Number}
-    },
-    isPaid: {
-        type: Boolean,
-        required: true,
-        default: false,
-    },
-    paidAt: {
-        type: Date,
-    },
-    isDelivered: {
-        type: Boolean,
-        required: true,
-        default: false,
-    },
-    deliveredAt: {
-        type: Date,
-    }
-}, {
-    timestamps: true,
-})
 
-const Order = mongoose.model("Order", orderSchema)
+    paidAt: {
+      type: Date,
+    },
+
+    deliveredAt: {
+      type: Date,
+    },
+    transactionHash: {
+      type: String,
+      required: true,
+    },
+    account: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Order = mongoose.model("Order", orderSchema);
 Order.watch().on("change", (data) => {
-    
-    if (data.operationType === "insert") {
-        io.emit("newOrder", data.fullDocument);
-    }
-})
-module.exports = Order
+  if (data.operationType === "insert") {
+    io.emit("newOrder", data.fullDocument);
+  }
+});
+module.exports = Order;
